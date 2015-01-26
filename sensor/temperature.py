43238e93e5603cdc2964e1be6c85f865b6bcc3b2
Robinson D. S. Santos <engenharia_robinson@inobram.com.br>
2015-01-26 16:37:42 -0200

    Added temperature simulator

---
+++ sensor/temperature.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+def randomize():
+    return round(random.uniform(0,100), 2)
+
+class Temperature(object):
+    def __init__(self):
+        pass
+
+    def get_value(self):
+        return randomize()
