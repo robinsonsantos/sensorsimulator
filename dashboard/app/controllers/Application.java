d5ba895167524584eea89ec693503857fa05058d
Robinson D. S. Santos <stanleymagrao@gmail.com>
2015-01-28 16:39:27 -0200

    Added crud

--- dashboard/app/controllers/Application.java
+++ dashboard/app/controllers/Application.java
@@ -1,8 +1,14 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
+import org.h2.engine.Database;
+
+import models.User;
+import play.*;
+import play.db.ebean.Model;
+import play.libs.Json;
+import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
@@ -10,5 +16,26 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-
+    
+    public static Result createUser() {
+    	//User user = Json.fromJson(request().body().asJson(), User.class);    	
+    	//user.save();
+    	return ok("Success");
+    }
+    
+    public static Result readUser(Long id) {    	
+    	return TODO;
+    }    
+    
+    public static Result readUsers() {
+    	return TODO;
+    }
+    
+    public static Result updateUser() {
+    	return TODO;
+    }
+    
+    public static Result deleteUser() {
+    	return TODO;    	
+    }
 }
