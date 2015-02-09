79bf50233ad4cd61192efa02e16b9f6c9a5a538e
Robinson D. S. Santos <stanleymagrao@gmail.com>
2015-02-09 21:27:02 -0200

    Added Device model

---
+++ dashboard/app/models/Device.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import play.db.ebean.*;
+
+@Entity
+public class Device extends Model {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+    public String token;
+    public String name;
+    public String value;
+
+    public static Model.Finder<Long, Device> find =
+                    new Model.Finder<Long, Device>(Long.class, Device.class);        
+}
