10b13ce1807f0945f82ea452e8069ac543630232
Robinson D. S. Santos <stanleymagrao@gmail.com>
2015-02-11 22:12:35 -0200

    device model in own folder

---
+++ dashboard/app/controllers/Devices.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.util.List;
+import java.util.UUID;
+
+import models.Device;
+
+import play.libs.Json;
+import play.mvc.*;
+import views.html.devices_.*;
+import play.data.Form;
+
+
+public class Devices extends Controller {
+
+    public static Result index() {
+        return ok(index.render());
+    }
+    
+    public static Result createDevice() {
+        //Device device = Json.fromJson(request().body().asJson(), Device.class);
+        Device device = Form.form(Device.class).bindFromRequest().get(); 
+        device.setUuid(UUID.randomUUID().toString());
+    	device.save();
+    	//return ok("Success");
+        return redirect(routes.Application.index());
+    }
+
+    public static Result readDevices() {
+    	List<Device> devices = Device.find.all();
+    	return ok(Json.toJson(devices));
+    }
+}
