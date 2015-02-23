a1b8904d28ecb22b8551800ca58b4ad1770e6dbd
Robinson D. S. Santos <stanleymagrao@gmail.com>
2015-02-23 09:42:06 -0300

    Added constraint in Device model

--- dashboard/app/models/Device.java
+++ dashboard/app/models/Device.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import play.db.ebean.Model;
+import play.data.validation.Constraints.Required;
 
 @Entity
 public class Device extends Model {
@@ -14,8 +15,11 @@ public class Device extends Model {
     @Id
     @GeneratedValue
     public Long id;
+    @Required(message="Give us a UUID!")
     public String uuid;
+    @Required(message="Give us a name!")
     public String name;
+    @Required(message="Give us a Value Type!")
     public String valueType;
 
     public static Model.Finder<Long, Device> find =
